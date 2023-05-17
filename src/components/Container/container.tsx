8015fc0203fb66a41172725eccad1b329cfeb4ff
xiangxingqiang <xiangxingqiang1@tal.com>
2023-05-17 15:46:14 +0800

    5.17

--- src/components/Container/container.tsx
+++ src/components/Container/container.tsx
@@ -2,14 +2,27 @@ import React, { ReactNode } from 'react';
 import type { FC } from 'react';
 
 const style = {
+  position: 'relative',
   width: 350,
   height: 300,
+  padding: 10,
   borderRadius: 15,
   backgroundColor: '#F4F5F5',
-  color: 'black'
+  color: 'black',
 };
-const Container: FC<{ children: ReactNode }> = ({ children }) => {
-  return <div style={style}>{children}</div>;
+const Container: FC<{
+  children: ReactNode;
+  className?: string;
+  center?: boolean;
+}> = ({ children, className, center = false }) => {
+  const centerStyle = center
+    ? { display: 'flex', justifyContent: 'center', alignItems: 'center' }
+    : {};
+  return (
+    <div className={className} style={{ ...style, ...centerStyle }}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
--- src/pages/Css/index.tsx
+++ src/pages/Css/index.tsx
@@ -2,16 +2,34 @@ import React from 'react';
 import type { FC } from 'react';
 import { Typography } from '@douyinfe/semi-ui';
 import Container from '../../components/Container/container';
+import styles from './index.module.less';
 
 const { Title } = Typography;
+
 const Index: FC = () => {
   return (
     <>
       <Title heading={4}>动画</Title>
-      <div>
-      <Container>
-        THIS IS  A CSS  ANIMATION
-      </Container>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <Container className={styles.container}>
+          <div className={styles.main}>
+            <img
+              src={
+                'https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/root-web-sites/abstract.jpg'
+              }
+              alt=''
+              width={100}
+              height={70}
+              className={styles.img}
+            />
+            <div className={styles.circle}></div>
+            <div className={styles.triangle}></div>
+            <div className={styles.square}></div>
+          </div>
+        </Container>
+        <Container center>
+          <div className={styles.tabBox}></div>
+        </Container>
       </div>
     </>
   );
